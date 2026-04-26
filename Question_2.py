1d3a3f76b5c343ff6f0295bf6a843ed7959e5e7e
Sawyer McKenney <sawyermckenney@gmail.com>
2026-04-26 17:46:41 -0600

    Final commit, added images, gif and the final report

--- Question_2.py
+++ Question_2.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.animation import PillowWriter
+
 
 
 M = 1
@@ -65,16 +67,16 @@ def CLpen(t,state):
 #initang = np.random.uniform(0,10)
 #print(initang)
 #init = [0.0, 0.0, np.radians(initang), 0.0]
-initial_angles = [2, 5, 10]
+initial_angles = [np.radians(np.random.uniform(0,10)), np.radians(np.random.uniform(0,10)), np.radians(np.random.uniform(0,10))]
 results = []
 for angle in initial_angles:
-    init = [0.0, 0.0, np.radians(angle), 0.0]
+    init = [0.0, 0.0, angle, 0.0]
 
     t = (0,10)
     sol = solve_ivp(CLpen,t,init, max_step=0.005, rtol=1e-8)
     results.append(sol)
 
-fig, axes = plt.subplots(2,1, figsize=(10,8), sharex=True)
+#fig, axes = plt.subplots(2,1, figsize=(10,8), sharex=True)
 
 for i, sol in enumerate(results):
 
@@ -82,127 +84,64 @@ for i, sol in enumerate(results):
     x = sol.y[0] # cart position
     xdot = sol.y[1] # cart velocity
     theta = sol.y[2] # pend angle
-    #thetadot = sol.y[3] # pend velocity
-    axes[0].plot(t,x, label=f'{initial_angles[i]} deg')
-    axes[1].plot(t,np.degrees(theta), label=f'{initial_angles[i]} deg')
+    thetadot = sol.y[3] # pend velocity
+
+
+    fig, axes = plt.subplots(2,1, figsize=(10,8), sharex=True)
+
+    axes[0].plot(t,x, label='X Cart Pos')
+    axes[0].plot(t,xdot, label='xdot cart velocity', linestyle='--')
+    axes[0].set_ylabel('Cart')
+    axes[0].legend()
+    axes[0].grid(True, alpha = 0.3)
+
+    axes[1].plot(t,np.degrees(theta), label='Pendulum Angle')
+    axes[1].plot(t,np.degrees(thetadot), label='Pendulum angular velocity', linestyle='--')
+    axes[1].set_ylabel('Pendulum')
+    axes[1].set_xlabel('Time (s)')
+    axes[1].legend()
+    axes[1].grid(True, alpha = 0.3)
+
+
+    # axes[0].plot(t,x, label=f'{np.degrees(initial_angles[i])} deg')
+    # axes[1].plot(t,np.degrees(theta), label=f'{np.degrees(initial_angles[i])} deg')
     max_angle = np.max(np.abs(np.degrees(theta)))
+    angle_deg = np.round(np.degrees(initial_angles[i]), decimals=2)
+    plt.suptitle(f'LQR controller for inverted pendulum with initial angle {angle_deg} degrees')
+    plt.savefig("Question_2_" +str(angle_deg) +".png")
+    plt.show()
+    framesPerSec = 60
+    frame_time = np.arange(0, t[-1], 1/framesPerSec)
+    x_frames = np.interp(frame_time, t, x)
+    theta_frames = np.interp(frame_time, t, theta)
 
-axes[1].axhline(20, linestyle='--', color='red')
-axes[1].axhline(-20, linestyle='--', color='red') # plot the 20 degree threshold
-#axes[0].plot(t,x, label='X Cart Pos')
-#axes[0].plot(t,xdot, label='xdot cart velocity', linestyle='--')
-axes[0].set_ylabel('Cart')
-axes[0].legend()
-axes[0].grid(True, alpha = 0.3)
+    fig2, ax = plt.subplots(figsize=(8,6))
+    cw, ch = 0.3, 0.18
+    cart = plt.Rectangle((0, -ch), cw, ch, color='blue')
+    ax.add_patch(cart)
+    rod, = ax.plot([], [], lw=2, color='red')
+    bob, = ax.plot([], [], 'o', color='green', ms=10)
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-0.5, 1)
+    ax.set_aspect('equal')
+    plt.suptitle(f'LQR controller for inverted pendulum with initial angle {angle_deg} degrees')
+    def update(frame):
+        xc = x_frames[frame]
+        thetac = theta_frames[frame]
+        cart.set_xy((xc - cw/2, -ch))
+        bx = xc+ L * np.sin(thetac)
+        by = L * np.cos(thetac)
+        rod.set_data([xc, bx], [0, by])
+        bob.set_data([bx], [by])
+        return cart, rod, bob
 
-#axes[1].plot(t,np.degrees(theta), label='Pendulum Angle')
-#axes[1].plot(t,np.degrees(thetadot), label='Pendulum angular velocity', linestyle='--')
-axes[1].set_ylabel('Pendulum')
-axes[1].set_xlabel('Time (s)')
-axes[1].legend()
-axes[1].grid(True, alpha = 0.3)
-
-# plt.suptitle('LQR controller for inverted pendulum')
-# plt.show()
-
-sol = results[-1]   # pick the 10° case (hardest one)
-
-t = sol.t
-x = sol.y[0]
-theta = sol.y[2]
-
-framesPerSec = 60
-frame_time = np.arange(0, t[-1], 1/framesPerSec)
-x_frames = np.interp(frame_time, t, x)
-theta_frames = np.interp(frame_time, t, theta)
-
-fig2, ax = plt.subplots(figsize=(8,6))
-cw, ch = 0.3, 0.18
-cart = plt.Rectangle((0, -ch), cw, ch, color='blue')
-ax.add_patch(cart)
-rod, = ax.plot([], [], lw=2, color='red')
-bob, = ax.plot([], [], 'o', color='green', ms=10)
-ax.set_xlim(-1, 1)
-ax.set_ylim(-0.5, 1)
-ax.set_aspect('equal')
-def update(frame):
-    xc = x_frames[frame]
-    thetac = theta_frames[frame]
-    cart.set_xy((xc - cw/2, -ch))
-    bx = xc+ L * np.sin(thetac)
-    by = L * np.cos(thetac)
-    rod.set_data([xc, bx], [0, by])
-    bob.set_data([bx], [by])
-    return cart, rod, bob
-
-anim = FuncAnimation(fig2, update, frames=len(frame_time), blit=True, interval=1000/framesPerSec)
-plt.show()
-
-
-# bonus 
-
-def CLpenstep(t,state):
-    
-    if t >0.5:
-        step = 1
-    else: 
-        step = 0
-    
-    
-    F = float(-K @ state) + step
-    return nonlinearpen(state, F, M, m, L, b, g)
+    anim = FuncAnimation(fig2, update, frames=len(frame_time), blit=True, interval=1000/framesPerSec)
+    anim.save("Question_2_" +str(angle_deg) +".gif", writer=PillowWriter(fps=framesPerSec))
+    plt.show()
 
 
 
-init = [0,0,0,0]
-t=(0,10)
-stepout = solve_ivp(CLpenstep,t,init,max_step=0.005, rtol=1e-8)
 
-t     = sol.t
-x     = sol.y[0] # cart position
-xdot  = sol.y[1] # cart velocity
-theta = sol.y[2] # pend angle
-thetadot = sol.y[3] # pend velocity
 
-fig, axes = plt.subplots(2,1, figsize=(10,8), sharex=True)
 
-axes[0].plot(t,x, label='X Cart Pos')
-axes[0].plot(t,xdot, label='xdot cart velocity', linestyle='--')
-axes[0].set_ylabel('Cart')
-axes[0].legend()
-axes[0].grid(True, alpha = 0.3)
 
-axes[1].plot(t,np.degrees(theta), label='Pendulum Angle')
-axes[1].plot(t,np.degrees(thetadot), label='Pendulum angular velocity', linestyle='--')
-axes[1].set_ylabel('Pendulum')
-axes[1].set_xlabel('Time (s)')
-axes[1].legend()
-axes[1].grid(True, alpha = 0.3)
-
-plt.show()
-framesPerSec = 60
-frame_time = np.arange(0, t[-1], 1/framesPerSec)
-x_frames = np.interp(frame_time, t, x)
-theta_frames = np.interp(frame_time, t, theta)
-
-fig2, ax = plt.subplots(figsize=(8,6))
-cw, ch = 0.3, 0.18
-cart = plt.Rectangle((0, -ch), cw, ch, color='blue')
-ax.add_patch(cart)
-rod, = ax.plot([], [], lw=2, color='red')
-bob, = ax.plot([], [], 'o', color='green', ms=10)
-ax.set_xlim(-1, 1)
-ax.set_ylim(-0.5, 1)
-ax.set_aspect('equal')
-def update(frame):
-    xc = x_frames[frame]
-    thetac = theta_frames[frame]
-    cart.set_xy((xc - cw/2, -ch))
-    bx = xc+ L * np.sin(thetac)
-    by = L * np.cos(thetac)
-    rod.set_data([xc, bx], [0, by])
-    bob.set_data([bx], [by])
-    return cart, rod, bob
-
-anim = FuncAnimation(fig2, update, frames=len(frame_time), blit=True, interval=1000/framesPerSec)
-plt.show()
